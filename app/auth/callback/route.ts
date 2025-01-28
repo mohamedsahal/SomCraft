12f22bf3129df2dcebfc52057aaaecff05b4a7b4
Mohamed Sahal <your.email@example.com>
2025-01-28 09:58:03 +0300

    fixed

--- app/auth/callback/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  // Simply redirect to origin after sign in process
-  return NextResponse.redirect(requestUrl.origin)
-}
